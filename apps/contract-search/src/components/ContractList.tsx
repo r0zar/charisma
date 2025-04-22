aadb904a3d06a658777cdf361ebaec75da3139ff
Ross Ragsdale <ross@pointblankdev.com>
2025-04-22 01:03:15 -0500

    :art: Update

--- apps/contract-search/src/components/ContractList.tsx
+++ apps/contract-search/src/components/ContractList.tsx
@@ -644,7 +644,7 @@ export default function ContractList({ isDevelopment, initialSearches }: Contrac
                                             )}
 
                                             {/* Delete Button */}
-                                            <button
+                                            {isDevelopment && <button
                                                 onClick={() => handleDeleteSearch(search.id)}
                                                 className="ml-2 p-1.5 text-red-500 hover:bg-red-50 dark:hover:bg-red-900/20 rounded-full"
                                                 title="Delete search"
@@ -652,7 +652,7 @@ export default function ContractList({ isDevelopment, initialSearches }: Contrac
                                                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                                                 </svg>
-                                            </button>
+                                            </button>}
                                         </div>
                                     </div>
 
