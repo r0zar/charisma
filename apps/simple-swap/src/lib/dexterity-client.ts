420d2ee8eeea6f2e0f898b3e74f9e7650215491c
Ross Ragsdale <ross@pointblankdev.com>
2025-05-06 21:41:59 -0500

    :art: Update

--- apps/simple-swap/src/lib/dexterity-client.ts
+++ apps/simple-swap/src/lib/dexterity-client.ts
@@ -1004,7 +1004,7 @@ export class Dexterity {
                 network: 'mainnet',
                 postConditions: options.postConditions,
                 postConditionMode: options.postConditionMode || PostConditionMode.Deny,
-                fee: options.fee || 250
+                fee: options.fee || 300
             };
 
             if (options.nonce) {
