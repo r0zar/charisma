8b7eeb76baf88ab56ae866b55f646fa5fb7d03be
Ross Ragsdale <ross@pointblankdev.com>
2025-05-20 02:50:34 -0500

    :chart_with_upwards_trend: Add sentry

---
+++ apps/dex-cache/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://52e31c3657d0ab8606aab80e06cbdfb1@o4508108453642240.ingest.us.sentry.io/4509354363846656",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
