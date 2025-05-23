729fb353e34b7f5ced8f5a3ebc190e2c7bdd1304
Ross Ragsdale <ross@pointblankdev.com>
2025-05-23 04:27:19 -0500

    :art: Update

--- apps/simple-swap/src/components/shop/ShopTable.tsx
+++ apps/simple-swap/src/components/shop/ShopTable.tsx
@@ -579,7 +579,7 @@ const ShopTable: React.FC<ShopTableProps> = ({ items, subnetTokens }) => {
                 >
                     {/* Search and Create Button */}
                     <div className="flex gap-2 flex-1">
-                        <div className="relative flex-1">
+                        <div className="relative flex-1 mr-2">
                             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
                             <Input
                                 placeholder="Search items, creator names, or addresses..."
