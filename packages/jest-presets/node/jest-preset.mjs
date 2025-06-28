4b3b6398e938f30c457d294ae102d8492004ec82
Ross Ragsdale <ross@pointblankdev.com>
2025-06-28 08:29:17 -0500

    :white_check_mark: Remove coverage threshold

--- packages/jest-presets/node/jest-preset.mjs
+++ packages/jest-presets/node/jest-preset.mjs
@@ -23,10 +23,10 @@ const config = {
   coverageReporters: ["text", "json", "html", "lcov"],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     },
   },
 };
