09d2bc42ac263763fbcd410f7152f20237c8b8d7
Ross Ragsdale <ross@pointblankdev.com>
2025-06-28 08:37:42 -0500

    :art: Config updates

--- apps/dex-cache/postcss.config.mjs
+++ apps/dex-cache/postcss.config.mjs
@@ -1,7 +1,8 @@
-import tailwindcss from '@tailwindcss/postcss'
-
+/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [tailwindcss],
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
 
 export default config;
