41dc260932cbd7b8f7cd78a1c027f37e1581590a
Ross Ragsdale <ross@pointblankdev.com>
2025-07-09 03:24:43 -0500

    :art: Update

--- apps/bot-manager/vitest.config.ts
+++ apps/bot-manager/vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   test: {
     environment: 'happy-dom',
     setupFiles: ['./tests/setup.ts'],
